cb6b0ff176408f21205935c42bc05de978b70bd9
Leo Mancini <mancini@leo.gd>
2025-07-13 23:21:50 -0700

    Updating animation duration

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 // Animation constants
-export const ANIMATION_DURATION = 0.2; // seconds
-export const ANIMATION_DURATION_MS = 200; // milliseconds
+export const ANIMATION_DURATION = 0.25; // seconds
 
 // Animation timing strings for CSS
 export const FADE_TRANSITION = `opacity ${ANIMATION_DURATION}s ease-in-out`;
